df542dd6dee2c3ebc29ce1520dc63ef68a4d5d91
MustafaKaanK <mkaankorkut@gmail.com>
2024-05-23 00:03:08 +0300

    production

--- pages/Result.js
+++ pages/Result.js
@@ -24,7 +24,7 @@ const Result = () =>{
     } 
       const timeout = setTimeout(() => {
         setAnimationTrigger(false);
-      }, 1182);
+      }, 1252);
     
       return () => clearTimeout(timeout);
     
