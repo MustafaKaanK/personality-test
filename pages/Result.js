e2700806ed6328f1756317c381380e897bf31595
MustafaKaanK <mkaankorkut@gmail.com>
2024-05-27 20:36:55 +0300

    Result screen animation adjusted

--- pages/Result.js
+++ pages/Result.js
@@ -24,7 +24,7 @@ const Result = () =>{
     } 
       const timeout = setTimeout(() => {
         setAnimationTrigger(false);
-      }, 1502);
+      }, 1102);
     
       return () => clearTimeout(timeout);
     
