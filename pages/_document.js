b349f4809ffaf98462145c3130d865abdc4eef55
MustafaKaanK <mkaankorkut@gmail.com>
2024-05-27 07:01:52 +0300

    translate notification disabled

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">  {/* Set language to English */}
         <Head>
-          <meta name="google" content="notranslate" />
+          <meta name="google" content="notranslate" />  {/* Prevent Google Translate */}
         </Head>
         <body>
           <Main />
